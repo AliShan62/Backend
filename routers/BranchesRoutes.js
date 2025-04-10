4999100d4899843ab7ffc3eb7e34156ce4e87cb4
Ali Shan <alishanit62@gmail.com>
2025-04-10 07:50:35 +0500

    first commit

--- routers/BranchesRoutes.js
+++ routers/BranchesRoutes.js
@@ -10,7 +10,8 @@ const {
 
 // Define child routes under the parent route `/api/v1/branches`
 router.post("/create", createBranch); // POST to `/api/v1/branches/create` for creating a new branch
-router.get("/", getBranchesByCompany); // GET to `/api/v1/branches/` for getting all branches
+//Updated route: Get all branches for a specific company
+router.get("/company/:companyId", getBranchesByCompany); // GET to `/api/v1/branches/` for getting all branches
 router.get("/:branchId", getSingleBranch); // GET to `/api/v1/branches/:branchId` for getting a single branch by ID
 router.put("/:branchId", updateBranch); // PUT to `/api/v1/branches/:branchId` for updating a branch
 router.delete("/:branchId", deleteBranch); // DELETE to `/api/v1/branches/:branchId` for deleting a branch
